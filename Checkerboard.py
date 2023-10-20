26c8f4dfb7d33d242767039cb64e36c4e9d81017
dyl-frank <dylan.frank@tamu.edu>
2023-10-20 14:20:20 -0500

    Fixed checkerboard algorithm to avoid vertical stripes. Checkerboard is now black and white

--- Checkerboard.py
+++ Checkerboard.py
@@ -1,30 +1,22 @@
-from turtle import color
 import cv2
 import numpy as np
 
-sizeBoard=int(input('Hello! How large is your checkerboard? '))
-nSquares=int(input('Ok. How many squares would you like to add? '))
-sizeSquare= int(sizeBoard/nSquares)
+sizeBoard = int(input('Hello! How large is your checkerboard? '))
+nSquares = int(input('Ok. How many squares would you like to add? '))
+sizeSquare = int(sizeBoard / nSquares)
 
-darkColor=(0,0,255)
-lightColor=(0,255,0)
-nowColor=darkColor
-while True:
-    frame=np.zeros([sizeBoard,sizeBoard,3], dtype= np.uint8)
+darkColor = (0, 0, 0)
+lightColor = (255, 255, 255)
 
-    for row in range(0,nSquares,1):
-        for column in range(0,nSquares,1):
-            frame[sizeSquare*row:sizeSquare*(row+1),sizeSquare*column:sizeSquare*(column+1)]=nowColor
-            if nowColor == darkColor:
-                nowColor=lightColor
-            else:
-                nowColor=darkColor
-        if nowColor==darkColor:
-            nowColor=lightColor
+frame = np.zeros([sizeBoard, sizeBoard, 3], dtype=np.uint8)
+
+for row in range(nSquares):
+    for column in range(nSquares):
+        if (row + column) % 2 == 0:
+            frame[sizeSquare * row:sizeSquare * (row + 1), sizeSquare * column:sizeSquare * (column + 1)] = darkColor
         else:
-            nowColor=darkColor
+            frame[sizeSquare * row:sizeSquare * (row + 1), sizeSquare * column:sizeSquare * (column + 1)] = lightColor
 
-
-    cv2.imshow('Checker Board', frame)
-    if cv2.waitKey(1) & 0xff ==ord('q'):
-        break
+cv2.imshow('Checker Board', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
